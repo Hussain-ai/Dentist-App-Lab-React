cb69169c0b964e925172862c8f4bf04d997aecf6
Hussain-ai <huser.adraj@gmail.com>
2022-11-04 23:43:51 +0300

    final update

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Home from './components/Home'
+import Procedures from './components/Procedures'
+import Contact from './components/Contact'
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+
+const ProceduresArray = [
+  'General Checkups',
+  'Teeth Cleaning',
+  'Cavity Screenings',
+  'Cavity Fillings',
+  'Chipped Tooth Fixings',
+  'Tooth Removal',
+  'Root Canals',
+]
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <nav>
+            <Link to="/">Home</Link>&nbsp;
+            <Link to="/procedures">Procedures</Link>&nbsp;
+            <Link to="/contact">Contact</Link>&nbsp;
+          </nav>
+          {/* <Home/> */}
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/procedures" element={<Procedures procedures={ProceduresArray}></Procedures>}></Route>
+            <Route path="/contact" element={<Contact></Contact>}></Route>
+          </Routes>
+        </Router>
+      </div>
+    )
+  }
+}
---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Dentist Web Application</h1>
+        <p>Welcome to my Dentist Website</p>
+      </div>
+    )
+  }
+}
